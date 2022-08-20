323fc9cb7e08bd95c84808aba8524629f82d93c6
shaquib khan <connectshaquib@gmail.com>
2022-08-20 16:01:54 +0530

    fixed navbar issue for mobile devices

---
+++ action.js
@@ -0,0 +1,7 @@
+// Grab HTML Elements
+const btn = document.querySelector("button.mobile-menu-button");
+const menu = document.querySelector(".mobile-menu");
+
+btn.addEventListener("click", () => {
+	menu.classList.toggle("hidden");
+});
